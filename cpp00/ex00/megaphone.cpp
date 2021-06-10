6fec12fb7452e83747f34f2923bfba53e7f59260
Jonny Saykosy <josayko@pm.me>
2021-06-10 16:54:19 +0200

    cpp00/ex00 done

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
 #include <string>
 
-//
-// Arg: a string to be converted in uppercase
-// Dev: Trim trailing whitespaces and convert to uppercase
-// Ret: uppercase string
-///
+// ARG: a string to be converted in uppercase
+// DEV: Trim trailing whitespaces and convert all characters to uppercase
+// RET: the initial ARG converted to an uppercase string
 std::string megaphone(std::string str) {
   str.erase(0, str.find_first_not_of(" \t"));
   str.erase(str.find_last_not_of(" \t") + 1);
-  for (unsigned long i = 0; i < str.length(); ++i)
+  for (unsigned long i = 0; i < str.length(); ++i) {
     str[i] = std::toupper(str[i]);
+  }
   return str;
 }
 
@@ -18,13 +17,14 @@ int main(int argc, char *argv[]) {
   if (argc > 1) {
     for (int i = 1; i < argc; ++i) {
       std::cout << megaphone(static_cast<std::string>(argv[i]));
-      if (i != argc - 1)
+      if (i != argc - 1) {
         std::cout << " ";
-      else
+      } else {
         std::cout << std::endl;
+      }
     }
-  }
-  else
+  } else {
     std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  }
   return 0;
 }
