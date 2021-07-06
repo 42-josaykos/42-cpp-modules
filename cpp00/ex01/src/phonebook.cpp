e9d349a6fae6f7d6d368b8445bade2cf23acbdef
Jonny Saykosy <josayko@pm.me>
2021-07-06 17:44:06 +0200

    update cpp00/ex01: add prompt

--- cpp00/ex01/src/phonebook.cpp
+++ cpp00/ex01/src/phonebook.cpp
@@ -3,7 +3,9 @@
 
 int main(void) {
   std::string input;
-  std::cout << "Input: ";
+  std::cout << "--- Phonebook ---" << std::endl;
+  std::cout << "Available commands: EXIT, ADD, SEARCH." << std::endl;
+  std::cout << "> ";
   std::getline(std::cin, input);
 
   std::cout << "Result: " << input << std::endl;
