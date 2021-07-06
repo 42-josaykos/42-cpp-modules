448c778c56f8ea1e457e12f267cdbbe2d31b186d
Jonny Saykosy <josayko@pm.me>
2021-07-06 17:39:01 +0200

    update cpp00/ex01

--- cpp00/ex01/src/phonebook.cpp
+++ cpp00/ex01/src/phonebook.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <string>
 
-int main() {
+int main(void) {
+  std::string input;
+  std::cout << "Input: ";
+  std::getline(std::cin, input);
+
+  std::cout << "Result: " << input << std::endl;
   return 0;
 }
