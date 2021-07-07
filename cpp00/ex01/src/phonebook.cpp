0c96de6bef273e4f6ea5567d1298931d61a50ea2
Jonny Saykosy <josayko@pm.me>
2021-07-07 17:09:00 +0200

    exit ok

--- cpp00/ex01/src/phonebook.cpp
+++ cpp00/ex01/src/phonebook.cpp
@@ -6,13 +6,24 @@ int main(void) {
   std::string input;
   std::cout << "--- Phonebook ---" << std::endl;
   std::cout << "Available commands: EXIT, ADD, SEARCH." << std::endl;
-  std::cout << "> ";
-  std::getline(std::cin, input);
 
-  if (input.length() > 10) {
-    input.replace(9, 1, ".");
+  while (1) {
+    std::cout << "> ";
+    std::getline(std::cin, input);
+
+    if (std::cin.eof() || input.compare("EXIT") == 0) {
+      std::cout << "EXIT" << std::endl;
+      return 0;
+    } else {
+      std::cout << "Error: invalid command." << std::endl;
+      std::cout << "Available commands: EXIT, ADD, SEARCH." << std::endl;
+    }
+
+    // if (input.length() > 10) {
+      // input.replace(9, 1, ".");
+    // }
+    // std::cout << std::setw(10) << input.substr(0, 10);
+    // std::cout << "|" << std::endl;
   }
-  std::cout << std::setw(10) << input.substr(0, 10);
-  std::cout << "|" << std::endl;
   return 0;
 }
