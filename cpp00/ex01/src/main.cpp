0c70f56790d22186c4c718937e4df46f58988ce3
Jonny Saykosy <josayko@pm.me>
2021-07-08 19:21:46 +0200

    update add_contact

--- cpp00/ex01/src/main.cpp
+++ cpp00/ex01/src/main.cpp
@@ -15,14 +15,13 @@ void print_list(Contact *phonebook) {
     phonebook[i].getInfos(data);
     std::cout << std::setw(10) << i;
     std::cout << "|";
-    for(int j = 0; j < 4; j++) {
+    for(int j = 0; j < 3; j++) {
       if (data[j].length() > 10) {
         data[j].replace(9, 1, ".");
       }
       std::cout << std::setw(10) << data[j].substr(0, 10);
-      if (j != 3) {
-        std::cout << "|";
-      } else {
+      std::cout << "|";
+      if (j == 2) {
         std::cout << std::endl;
       }
     }
@@ -32,7 +31,18 @@ void print_list(Contact *phonebook) {
 void add_contact(Contact *phonebook) {
   int i = 0;
   std::string data[11];
-  data[0] = "Jonny";
+  std::string input;
+  const std::string prompt[11] = {
+    "First Name ? ", "Last Name ? ", "Nickname ? ", "Login ? ",
+    "Postal Address ? ", "Email ? ", "Phone Number ? ", "Birthday ? ",
+    "Favorite Meal ? ", "Underwear Color ? ", "Darkest Secret ? "
+  };
+
+  for (int j = 0; j < 11; j++) {
+    std::cout << prompt[j];
+    std::getline(std::cin, input);
+    data[j] = input;
+  } 
   phonebook[i].setInfos(data);
 }
 
