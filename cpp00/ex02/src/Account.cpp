590a7f03ed3e59e605832ad4124bbb2c0b68030e
Jonny Saykosy <josayko@pm.me>
2021-07-12 12:52:44 +0200

    TODO: update displayStatus

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -1,53 +1,61 @@
 #include "Account.hpp"
+#include <iostream>
 
-int Account::getNbAccounts(void) {
-  int ret = 0;
-  return ret;
-}
+int Account::_nbAccounts;
+int Account::_totalAmount;
+int Account::_totalNbDeposits;
+int Account::_totalNbWithdrawals;
 
-int Account::getTotalAmount(void) {
-  int ret = 0;
-  return ret;
-}
+int Account::getNbAccounts(void) { return _nbAccounts; }
 
-int Account::getNbDeposits(void) {
-  int ret = 0;
-  return ret;
-}
+int Account::getTotalAmount(void) { return _totalAmount; }
 
-int Account::getNbWithdrawals(void) {
-  int ret = 0;
-  return ret;
-}
+int Account::getNbDeposits(void) { return _totalNbDeposits; }
+
+int Account::getNbWithdrawals(void) { return _totalNbWithdrawals; }
 
 void Account::displayAccountsInfos(void) { return; }
 
 Account::Account(int initial_deposit) {
-  (void)initial_deposit;
+  _nbAccounts++;
+  this->_accountIndex = _nbAccounts - 1;
+  _displayTimestamp();
+  std::cout << "index:" << this->_accountIndex;
+  std::cout << ";amount:" << initial_deposit;
+  std::cout << ";created";
+  std::cout << std::endl;
   return;
 }
 
 Account::~Account(void) { return; }
 
 void Account::makeDeposit(int deposit) {
-  (void)deposit;
+  this->_amount += deposit;
+  this->_nbDeposits++;
   return;
 }
 
 bool Account::makeWithdrawal(int withdrawal) {
   (void)withdrawal;
   bool ret = false;
+  this->_nbWithdrawals++;
   return ret;
 }
 
 int Account::checkAmount(void) const {
   int ret = 0;
-
   return ret;
 }
 
-void Account::displayStatus(void) const { return; }
+void Account::displayStatus(void) const {
+  std::cout << this->_nbDeposits << std::endl;
+  std::cout << this->_totalNbWithdrawals << std::endl;
+  return;
+}
 
-void _displayTimestamp(void) { return; }
+void Account::_displayTimestamp(void) {
+  std::cout << "[YYYYMMDD_HHMMSS] ";
+  return;
+}
 
 Account::Account(void) { return; }
