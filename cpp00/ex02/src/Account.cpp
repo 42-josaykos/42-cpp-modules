90eafc3b7b7d95d6e711c202e532022b5362dc6c
Jonny Saykosy <josayko@pm.me>
2021-07-12 11:48:27 +0200

    update ex02

--- cpp00/ex02/src/Account.cpp
+++ cpp00/ex02/src/Account.cpp
@@ -1,1 +1,53 @@
 #include "Account.hpp"
+
+int Account::getNbAccounts(void) {
+  int ret = 0;
+  return ret;
+}
+
+int Account::getTotalAmount(void) {
+  int ret = 0;
+  return ret;
+}
+
+int Account::getNbDeposits(void) {
+  int ret = 0;
+  return ret;
+}
+
+int Account::getNbWithdrawals(void) {
+  int ret = 0;
+  return ret;
+}
+
+void Account::displayAccountsInfos(void) { return; }
+
+Account::Account(int initial_deposit) {
+  (void)initial_deposit;
+  return;
+}
+
+Account::~Account(void) { return; }
+
+void Account::makeDeposit(int deposit) {
+  (void)deposit;
+  return;
+}
+
+bool Account::makeWithdrawal(int withdrawal) {
+  (void)withdrawal;
+  bool ret = false;
+  return ret;
+}
+
+int Account::checkAmount(void) const {
+  int ret = 0;
+
+  return ret;
+}
+
+void Account::displayStatus(void) const { return; }
+
+void _displayTimestamp(void) { return; }
+
+Account::Account(void) { return; }
