92a2fe74f92a2250d1672f84648f2cf7e08fe6b1
Jonny Saykosy <josayko@pm.me>
2021-07-26 18:26:45 +0200

    cpp01/ex02 done. TODO cpp01/ex03

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 
 int main(void) {
-  std::cout << "hello brain" << std::endl;
+  // string
+  std::string str = "HI THIS IS BRAIN";
+
+  // pointer to string
+  std::string* stringPTR = &str;
+
+  // reference to string
+  std::string& stringREF = str;
+
+  // address syntax: &str
+  std::cout << "Address of str: " << &str << std::endl;
+
+  // address syntax: stringPTR
+  std::cout << "Address of str using pointer stringPTR: " << stringPTR
+            << std::endl;
+
+  // address syntax: &stringREF
+  std::cout << "Address of str using reference stringREF: " << &stringREF
+            << std::endl
+            << std::endl;
+
+  // value syntax: str
+  std::cout << "Value of str: " << str << std::endl;
+
+  // value syntax: *stringPTR
+  std::cout << "Value of str using pointer stringPTR: " << *stringPTR
+            << std::endl;
+
+  // value syntax: stringREF
+  std::cout << "Value of str using reference stringREF: " << stringREF
+            << std::endl;
   return 0;
 }
