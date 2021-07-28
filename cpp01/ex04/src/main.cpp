4863cc6d844a3640f63707e13e2801c1b175e1df
Jonny Saykosy <josayko@pm.me>
2021-07-28 16:33:03 +0200

    cpp01/ex04: replace

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,6 +1,31 @@
-#include "replace.hpp"
+#include <fstream>
+#include <iostream>
+
+// Create a copy of filename with every occurence of s1 replace by s2
+int replace(std::string filename, std::string s1, std::string s2) {
+  if (filename.empty() || s1.empty() || s2.empty()) {
+    return 1;
+  }
+
+  std::ifstream ifs(filename.c_str());
+  if (!ifs.is_open()) {
+    std::cout << filename << " doesn't exist." << std::endl;
+    return 1;
+  }
+
+  std::string output;
+  while (std::getline(ifs, output)) {
+    std::cout << output;
+  }
+  ifs.close();
+
+  std::ofstream ofs((filename + ".replace").c_str());
+  ofs << output.c_str();
+  ofs.close();
+  return 0;
+}
 
 int main(void) {
-  std::cout << "hello replace !" << std::endl;
+  replace("test", "1", "2");
   return 0;
 }
