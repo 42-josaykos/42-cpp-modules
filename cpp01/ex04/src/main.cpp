1efb2c6c82f868899e4b1f96d787fa80e0b3dc38
Jonny Saykosy <josayko@pm.me>
2021-07-28 17:59:52 +0200

    cpp01/ex04: done

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -3,24 +3,9 @@
 #include <iostream>
 #include <sstream>
 
-// Create a copy of filename with every occurence of s1 replace by s2
-int replace(std::string filename, std::string s1, std::string s2) {
-  if (filename.empty() || s1.empty() || s2.empty()) {
-    return 1;
-  }
-
-  std::ifstream ifs(filename.c_str());
-  if (!ifs.is_open()) {
-    std::cout << filename << " doesn't exist." << std::endl;
-    return 1;
-  }
-
-  std::stringstream sstr;
-  sstr << ifs.rdbuf();
-  std::string str = sstr.str();
-
-  size_t pos = 0;
+std::string const get_output(std::string str, std::string s1, std::string s2) {
   std::string output;
+  size_t pos = 0;
 
   while (str.length() != 0) {
     if ((pos = str.find(s1)) != str.npos) {
@@ -36,6 +21,24 @@ int replace(std::string filename, std::string s1, std::string s2) {
       break;
     }
   }
+  return output;
+}
+
+// Create a copy of filename with every occurence of s1 replace by s2
+int replace(std::string filename, std::string s1, std::string s2) {
+  if (filename.empty() || s1.empty() || s2.empty()) {
+    return 1;
+  }
+
+  std::ifstream ifs(filename.c_str());
+  if (!ifs.is_open()) {
+    std::cout << filename << " doesn't exist." << std::endl;
+    return 1;
+  }
+  std::stringstream sstr;
+  sstr << ifs.rdbuf();
+  std::string str = sstr.str();
+  std::string output = get_output(str, s1, s2);
 
   std::ofstream ofs((filename + ".replace").c_str());
   ofs << output;
