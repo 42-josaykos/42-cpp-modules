61cef648c1309fffb1f70394c06ca9aa35224c4d
Jonny Saykosy <josayko@pm.me>
2021-08-01 17:02:39 +0200

    cpp02/ex03: ok

---
+++ cpp02/ex03/src/bsp.cpp
@@ -0,0 +1,31 @@
+#include "Point.hpp"
+
+float abs(float i) { return i < 0 ? -i : i; }
+
+Fixed area(Point const& a, Point const& b, Point const& c) {
+  // Area = | (ax(by - cy) + bx(cy - ay) + cx(ay - by)) / 2 |
+  float area =
+      (a.getX().toFloat() * (b.getY().toFloat() - c.getY().toFloat()) +
+       b.getX().toFloat() * (c.getY().toFloat() - a.getY().toFloat()) +
+       c.getX().toFloat() * (a.getY().toFloat() - b.getY().toFloat())) /
+      2.0f;
+  return Fixed(abs(area));
+}
+
+bool bsp(Point const& a, Point const& b, Point const& c, Point const& point) {
+  Fixed ABC = area(a, b, c);
+  Fixed PAB = area(point, a, b);
+  Fixed PBC = area(point, b, c);
+  Fixed PAC = area(point, a, c);
+
+  std::cout << "Area of triangle  ABC: " << ABC << std::endl;
+  std::cout << "Area of PAB: " << PAB << std::endl;
+  std::cout << "Area of PBC: " << PBC << std::endl;
+  std::cout << "Area of PAC: " << PAC << std::endl;
+  std::cout << "Total PAB + PBC + PAC: " << PAB + PBC + PAC << std::endl;
+
+  if (PAB + PBC + PAC == ABC) {
+    return true;
+  }
+  return false;
+}
--- cpp02/ex03/src/main.cpp
+++ cpp02/ex03/src/main.cpp
@@ -1,11 +1,21 @@
 #include "Point.hpp"
+#include "bsp.hpp"
 #include <iostream>
 
+// To verify: https://planetcalc.com/8108/
 int main(void) {
-  Point A(1.2, 2.3);
+  Point const A(0, 0);
+  Point const B(20, 0);
+  Point const C(10, 30);
+  // Point const P(16, 5); // Inside
+  Point const P(10, 15); // Not inside
 
-  std::cout << A.getX() << std::endl;
-  std::cout << A.getY() << std::endl;
+  if (bsp(A, B, C, P)) {
+    std::cout << "Point is inside" << std::endl;
+    return 1;
+  } else {
+    std::cout << "Point is not inside" << std::endl;
+  }
 
   return 0;
 }
