970381df58871fd4ed18398f01ad524caca3f96d
Jonny Saykosy <josayko@pm.me>
2021-08-30 12:21:10 +0200

    cpp06/ex02: add try catch in identify from reference

--- cpp06/ex02/src/main.cpp
+++ cpp06/ex02/src/main.cpp
@@ -28,7 +28,7 @@ Base* generate(void) {
   }
 }
 
-// dynamic_cast returns a NULL pointer if it fails
+// dynamic_cast to pointer returns a NULL pointer if it fails
 void identify(Base* p) {
   if (dynamic_cast<A*>(p)) {
     std::cout << "A" << std::endl;
@@ -40,14 +40,28 @@ void identify(Base* p) {
   return;
 }
 
+// dynamic_cast to reference returns a bad_cast exception if it fails
 void identify(Base& p) {
-  // identify(&p);
-  if (dynamic_cast<A*>(&p)) {
+  try {
+    p = dynamic_cast<A&>(p);
     std::cout << "A" << std::endl;
-  } else if (dynamic_cast<B*>(&p)) {
+    return;
+  } catch (std::exception& e) {
+    // std::cout << e.what() << std::endl;
+  }
+  try {
+    p = dynamic_cast<B&>(p);
     std::cout << "B" << std::endl;
-  } else if (dynamic_cast<C*>(&p)) {
+    return;
+  } catch (std::exception& e) {
+    // std::cout << e.what() << std::endl;
+  }
+  try {
+    p = dynamic_cast<C&>(p);
     std::cout << "C" << std::endl;
+    return;
+  } catch (std::exception& e) {
+    // std::cout << e.what() << std::endl;
   }
   return;
 }
