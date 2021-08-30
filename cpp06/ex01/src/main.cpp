7cd95d3832c81e609694765182036f4d4c5d744a
Jonny Saykosy <josayko@pm.me>
2021-08-30 11:01:11 +0200

    cpp06/ex01: update main test

--- cpp06/ex01/src/main.cpp
+++ cpp06/ex01/src/main.cpp
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 #include <stdint.h>
 
@@ -11,14 +12,20 @@ uintptr_t serialize(Data* ptr) { return reinterpret_cast<uintptr_t>(ptr); }
 
 Data* deserialize(uintptr_t raw) { return reinterpret_cast<Data*>(raw); }
 
-int main(void) {
+int main(int argc, char** argv) {
   Data*     idata = new Data;
   uintptr_t raw;
   Data*     odata;
 
-  idata->s1 = "fedcba";
-  idata->n = 256;
-  idata->s2 = "ABCDEF";
+  if (argc == 4) {
+    idata->s1 = argv[1];
+    idata->n = std::atoi(argv[2]);
+    idata->s2 = argv[3];
+  } else {
+    idata->s1 = "test";
+    idata->n = 42;
+    idata->s2 = "TEST";
+  }
 
   std::cout << "idata : " << idata->s1 << idata->n << idata->s2 << std::endl;
 
