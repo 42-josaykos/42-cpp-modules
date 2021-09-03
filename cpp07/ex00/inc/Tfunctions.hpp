23c0dc048c0295c72d87c4786a97654605e43e00
Jonny Saykosy <josayko@pm.me>
2021-09-03 14:52:10 +0200

    cpp07/ex00: done

--- cpp07/ex00/inc/Tfunctions.hpp
+++ cpp07/ex00/inc/Tfunctions.hpp
@@ -11,4 +11,22 @@ template <typename T> void swap(T& p1, T& p2) {
   return;
 }
 
+// Compares two variables and return the smallest one
+template <typename T> T& min(T& p1, T& p2) {
+  if (p1 < p2) {
+    return p1;
+  } else {
+    return p2;
+  }
+}
+
+// Compares two variables and return the biggest one
+template <typename T> T& max(T& p1, T& p2) {
+  if (p1 > p2) {
+    return p1;
+  } else {
+    return p2;
+  }
+}
+
 #endif
--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -8,8 +8,8 @@ int main(void) {
   int         b = 8;
   float       c = 42.2f;
   float       d = 2.24f;
-  Number      e(25);
-  Number      f(65);
+  Number      e(65);
+  Number      f(25);
   std::string s1 = "Hello";
   std::string s2 = "World";
 
@@ -36,5 +36,42 @@ int main(void) {
   swap(s1, s2);
   std::cout << "  s1: " << s1 << ", s2: " << s2 << std::endl;
   std::cout << std::endl;
+
+  std::cout << "--- min ---" << std::endl;
+  std::cout << "<int>" << std::endl;
+  std::cout << "  a: " << a << ", b: " << b << ", min: " << min(a, b)
+            << std::endl;
+
+  std::cout << "<float>" << std::endl;
+  std::cout << std::fixed << std::setprecision(2) << "  c: " << c
+            << ", d: " << d << ", min: " << min(c, d) << std::endl;
+
+  std::cout << "<Number>" << std::endl;
+  std::cout << std::fixed << std::setprecision(2) << "  e: " << e.getValue()
+            << ", f: " << f.getValue() << ", min: " << min(e, f).getValue()
+            << std::endl;
+
+  std::cout << "<string>" << std::endl;
+  std::cout << "  s1: " << s1 << ", s2: " << s2 << ", min: " << min(s1, s2)
+            << std::endl;
+  std::cout << std::endl;
+
+  std::cout << "--- max ---" << std::endl;
+  std::cout << "<int>" << std::endl;
+  std::cout << "  a: " << a << ", b: " << b << ", max: " << max(a, b)
+            << std::endl;
+
+  std::cout << "<float>" << std::endl;
+  std::cout << std::fixed << std::setprecision(2) << "  c: " << c
+            << ", d: " << d << ", max: " << max(c, d) << std::endl;
+
+  std::cout << "<Number>" << std::endl;
+  std::cout << std::fixed << std::setprecision(2) << "  e: " << e.getValue()
+            << ", f: " << f.getValue() << ", max: " << max(e, f).getValue()
+            << std::endl;
+
+  std::cout << "<string>" << std::endl;
+  std::cout << "  s1: " << s1 << ", s2: " << s2 << ", max: " << max(s1, s2)
+            << std::endl;
   return 0;
 }
