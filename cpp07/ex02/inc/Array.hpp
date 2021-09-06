7160fe502fddc55b76630dc639ace2fdde476cd5
Jonny Saykosy <josayko@pm.me>
2021-09-06 12:54:42 +0200

    cpp07/ex02: implement operator[]

--- cpp07/ex02/inc/Array.hpp
+++ cpp07/ex02/inc/Array.hpp
@@ -3,16 +3,78 @@
 
 #include <iostream>
 
-class Array {
+template <typename T> class Array {
 
 public:
   Array(void);
+  Array(unsigned int const& n);
   Array(Array const& src);
   virtual ~Array(void);
 
-  Array& operator=(Array const& rhs);
+  Array&   operator=(Array const& rhs);
+  T&       operator[](unsigned int i);
+  const T& operator[](unsigned int i) const;
 
 private:
+  int _size;
+  T*  _content;
 };
+
+template <typename T> Array<T>::Array(void) {
+  std::cout << "{ default ctor }" << std::endl;
+  this->_content = new T[0]();
+  this->_size = 0;
+  return;
+}
+
+template <typename T> Array<T>::Array(unsigned int const& n) {
+  std::cout << "{ size ctor }" << std::endl;
+  this->_content = new T[n]();
+  this->_size = n;
+
+  return;
+}
+
+template <typename T> Array<T>::Array(Array const& src) {
+  std::cout << "{ copy ctor }" << std::endl;
+  this->_content = new T[src._size];
+  this->_size = src._size;
+  for (int i = 0; i < this->_size; i++) {
+    this->_content[i] = src._content[i];
+  }
+  return;
+}
+
+template <typename T> Array<T>::~Array(void) {
+  std::cout << "{ dtor }" << std::endl;
+  delete[] this->_content;
+  return;
+}
+
+/******* operators ***********************************************************/
+
+template <typename T> Array<T>& Array<T>::operator=(Array const& rhs) {
+  if (this != &rhs) {
+    delete this->_content;
+    this->_content = new T[rhs._size];
+    this->_size = rhs._size;
+    for (int i = 0; i < this->_size; i++) {
+      this->_content[i] = rhs._content[i];
+    }
+  }
+  return *this;
+};
+
+template <typename T> T& Array<T>::operator[](unsigned int i) {
+  return this->_content[i];
+}
+
+template <typename T> const T& Array<T>::operator[](unsigned int i) const {
+  return this->_content[i];
+}
+
+template <typename T> std::ostream& operator<<(std::ostream& out, T& inst) {
+  return out << inst;
+}
 
 #endif
--- cpp07/ex02/src/main.cpp
+++ cpp07/ex02/src/main.cpp
@@ -1,7 +1,24 @@
 #include "Array.hpp"
 
 int main(void) {
-  std::cout << "--- Array ---" << std::endl;
-  Array ar = Array();
+  int* a = new int();
+  std::cout << *a << std::endl;
+
+  // Array<int>* ar = new Array<int>;
+  Array<int>* ar2 = new Array<int>(5);
+  Array<int>  ar3 = *ar2;
+
+  (*ar2)[0] = 42;
+  ar3[0] = 34;
+
+  // std::cout << *ar << std::endl;
+  std::cout << (*ar2)[0] << std::endl;
+  std::cout << ar3[0] << std::endl;
+
+  delete a;
+  // delete ar;
+  delete ar2;
+  // delete ar3;
+
   return 0;
 }
